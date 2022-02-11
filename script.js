80a4ab6a4fdf87533cdfb789545d3e0559500b6e
Tsahi-H <99398484+Tsahi-H@users.noreply.github.com>
2022-02-11 13:03:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function compute()
 {
-		let year = new Date().getFullYear();
-    let years = parseInt(document.getElementById("years").value); 
-    let amount = document.getElementById("principal").value;
-    let interest = document.getElementById("rate").value;
-    let interestCalc = document.getElementById("rate").value/100.;
-    let initialAmount = amount;
-		if ((isNaN(amount)) || (amount == "")) {
-    	alert("Please enter a valid number");    
-    } else {
-      amount = years * interestCalc * amount;
-      document.getElementById("s1").innerHTML = initialAmount;
-      document.getElementById("s2").innerHTML = Math.round(interest*100) / 100 + "%";
-      document.getElementById("s3").innerHTML = Math.round(amount*100) / 100;
-      document.getElementById("s4").innerHTML = (year+years);
-      document.getElementById("resultsTr").style.display = "";
+	let year = new Date().getFullYear();
+	let years = parseInt(document.getElementById("years").value); 
+	let amount = document.getElementById("principal").value;
+	let interest = document.getElementById("rate").value;
+	let interestCalc = document.getElementById("rate").value/100.;
+	let initialAmount = amount;
+	if ((isNaN(amount)) || (amount == "")) {
+    		alert("Please enter a valid number");    
+    	} else {
+		amount = years * interestCalc * amount;
+		document.getElementById("s1").innerHTML = initialAmount;
+		document.getElementById("s2").innerHTML = Math.round(interest*100) / 100 + "%";
+		document.getElementById("s3").innerHTML = Math.round(amount*100) / 100;
+		document.getElementById("s4").innerHTML = (year+years);
+		document.getElementById("resultsTr").style.display = "";
 	}
 }
         
